0cfff7e9bc0a014991f641d5381ebf19228dc494
Dario <astorgadm@gmail.com>
2017-10-08 19:13:36 -0300

    Update

--- app/src/main/java/com/example/dario/supermarketapp/MainActivity.java
+++ app/src/main/java/com/example/dario/supermarketapp/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.dario.supermarketapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -82,7 +83,8 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_gallery) { //SUPERMERCADOS
+            startActivity(new Intent(getApplicationContext(), ItemListActivity.class));
 
         } else if (id == R.id.nav_slideshow) {
 
